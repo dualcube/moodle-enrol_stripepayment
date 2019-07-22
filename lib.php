e0a01b57000032a8d0b5a478b0a0c33e2695c804
Ubuntu <ubuntu@ip-172-31-17-155.eu-central-1.compute.internal>
2019-07-22 13:37:11 +0000

    Fix

--- lib.php
+++ lib.php
@@ -413,14 +413,13 @@ class enrol_stripepayment_plugin extends enrol_plugin {
      * @param int $length
      * @return string
      */	
-	public function generate_random_string($length = 10) {
-		$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-		$characters_length = strlen($characters);
-		$random_string = '';
-		for ($i = 0; $i < $length; $i++) {
-			$random_string .= $characters[rand(0, $characters_length - 1)];
-		}
-		return $random_string;
-	}
-	
+    private function generate_random_string($length = 10) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $characters_length = strlen($characters);
+        $random_string = '';
+        for ($i = 0; $i < $length; $i++) {
+             $random_string .= $characters[rand(0, $characters_length - 1)];
+             }
+        return $random_string;
+        }
 }
