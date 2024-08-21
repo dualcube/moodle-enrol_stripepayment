b8cab0f17ee3e2d7ca15c687bfed12ea95d9eb21
Purnendu Dash <32759662+purnendudash@users.noreply.github.com>
2024-08-21 11:25:02 +0530

    Update enrol.php

--- enrol.php
+++ enrol.php
@@ -25,7 +25,7 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 defined('MOODLE_INTERNAL') || die();
-global $CFG;
+global $CFG, $PAGE;
 require_once($CFG->libdir . '/enrollib.php');
 require_once('Stripe/init.php');
 
