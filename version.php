38d04d3c46d2b7ae3609b71d11d6b4dae97b8558
Sagar Saha <ss2749968@gmail.com>
2025-07-08 19:01:23 +0530

    lang/en/enrol_stripepayment.php clean up

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
  */
 
 defined('MOODLE_INTERNAL') || die();
-$plugin->version   = 2025070701;
+$plugin->version   = 2025070702;
 $plugin->requires  = 2025041400;
 $plugin->component = 'enrol_stripepayment';
 $plugin->maturity  = MATURITY_STABLE;
