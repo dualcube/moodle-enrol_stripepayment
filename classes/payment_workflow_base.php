a20dee3fde0dbb94022cc210374b82f860d7e065
vulocartdev <vulocartdev@gmail.com>
2026-09-02 16:31:59 +0530

    fix

--- classes/payment_workflow_base.php
+++ classes/payment_workflow_base.php
@@ -19,7 +19,7 @@
  *
  * @package    enrol_stripepayment
  * @author     DualCube <admin@dualcube.com>
- * @copyright  2025 DualCube Team(https://dualcube.com)
+ * @copyright  2026 DualCube Team(https://dualcube.com)
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
@@ -50,7 +50,7 @@ use text_progress_trace;
  *
  * @package    enrol_stripepayment
  * @author     DualCube <admin@dualcube.com>
- * @copyright  2025 DualCube Team(https://dualcube.com)
+ * @copyright  2026 DualCube Team(https://dualcube.com)
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 abstract class payment_workflow_base extends plugin_base {
